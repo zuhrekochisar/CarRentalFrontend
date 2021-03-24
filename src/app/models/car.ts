e8a61fbae57cdfe9103f3d70cf3cfc516894c8ec
Zühre Koçhisar <zuhrekochisar@gmail.com>
2021-03-24 13:53:53 +0300

    Car Rental System-Front-End

---
+++ src/app/models/car.ts
@@ -0,0 +1,11 @@
+export interface Car {
+    carId:number;
+    brandId:number;
+    colorId:number;
+    modelYear:number;
+    dailyPrice:number;
+    descriptions:string;
+    carName:string;
+
+
+}
